144efe1ef964478a707d0f4c34c87d94b47898e7
wattnotions <shaneormonde@gmail.com>
2020-04-22 23:33:55 +0100

    made changes to try make example work

--- Video_Streamer.py
+++ Video_Streamer.py
@@ -17,6 +17,7 @@ def index():
 def gen(camera):
     while True:
         frame = camera.get_frame()
+        cv2.imshow('Input', frame)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
